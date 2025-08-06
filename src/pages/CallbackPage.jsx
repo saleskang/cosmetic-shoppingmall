7b1d158fce54a1a1bc807bba940757ad5358197a
saleskang <gsland153@gmail.com>
2025-08-06 16:22:52 +0900

    callback page modify

--- src/pages/CallbackPage.jsx
+++ src/pages/CallbackPage.jsx
@@ -1,9 +1,17 @@
+import { useSearchParams } from "react-router-dom";
+
 const CallbackPage = () => {
-    return (
-      <div>
-        <h2>카페24 인증 결과를 처리하는 페이지입니다.</h2>
-      </div>
-    );
-  };
-  
-  export default CallbackPage;
+  const [searchParams] = useSearchParams();
+  const code = searchParams.get("code");
+  const state = searchParams.get("state");
+
+  return (
+    <div>
+      <h2>카페24 인증 결과를 처리하는 페이지입니다.</h2>
+      <div>code: {code}</div>
+      <div>state: {state}</div>
+    </div>
+  );
+};
+
+export default CallbackPage;
